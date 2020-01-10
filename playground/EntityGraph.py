153c65803d56f72b6901a1ca5e5957276fe83a2e
SimonPreissner <simon.preissner@gmx.de>
2020-01-10 21:43:48 +0100

    implemented graph constructor (including a little output method)

--- playground/EntityGraph.py
+++ playground/EntityGraph.py
@@ -43,13 +43,13 @@ class EntityGraph():
                  "Well, I also think that he is nice.",
                  "Mary, however liked Tony even more than we do."]
             ]
-        self.graph = []
+        self.graph = [] # list characteristics are utilized by connect_graph()
         self.populate_graph()
         self.connect_graph()
 
-
     def __repr__(self):
         return "\n".join([str(t) for t in self.graph])
+
 
     def populate_graph(self):
         nlp = StanfordCoreNLP("http://corenlp.run/")
@@ -62,6 +62,8 @@ class EntityGraph():
                                                      "outputFormat": "json"})
                 entities = annotated['sentences'][0]['entitymentions']
                 for e in entities:
+                    # TODO change relations container from list to set?
+                    # TODO if so, then also change connect_graph()!
                     self.graph.append((ent_id,
                                        para_id,
                                        sent_id,
@@ -69,13 +71,54 @@ class EntityGraph():
                                        e['characterOffsetEnd'],
                                        [],
                                        e['text']
-                                       )
-                                      )
+                                      ))
                     ent_id += 1
 
     def connect_graph(self):
-        #TODO sentence-level links
+        """
+        #TODO docstring
+        """
+        """
+        As self.graph is a list and IDs are basically counters, the graph nodes
+        can be looked at in a "forward-only" fashion so that the algorithm runs
+        in n! steps for n entities (instead of n^2)
+        """
+        for e1 in self.graph:
+            for e2 in self.graph[e1[0]+1:]:
+                # all relations are symmetric -> they're added to both nodes
+                if e1[2] == e2[2]:
+                    # same sentence ID -> sentence-level link
+                    e1[5].append((e2[0], 0))
+                    e2[5].append((e1[0], 0))
+                if e1[6] == e2[6]:
+                    # same name -> context-level link
+                    e1[5].append((e2[0], 1))
+                    e2[5].append((e1[0], 1))
+                if e1[2] == 0 and e1[1] == e2[1]:
+                    # e1 in title sent. & same paragraph -> paragraph-level link
+                    e1[5].append((e2[0], 2))
+                    e2[5].append((e1[0], 2))
 
-        #TODO context-level links
-
-        #TODO paragraph-level links
+    def link_triplets(self):
+        """
+        Computes the set of relation triplets (e1, e2, rel_type) of a graph,
+        where e1 and e2 are two related entities and rel_type is their relation.
+        All 3 relation types are symmetric and are represented as two
+        one-directional edges in the EntityGraph object, but here only one of
+        a relation's two edges is included.
+        Relation types are coded as:
+        0 - sentence-level link
+        1 - context-level link
+        2 - paragraph-level link
+        :return: set of link triplets (e1, e2, rel_type)
+        """
+        relations = set()
+        for node in self.graph:
+            relations.update(set([ (node[0],r[0],r[1]) for r in node[5] ]))
+        result = set()
+        for e1,e2,rt in relations:
+            if (e2,e1,rt) not in result: # only keep one of the two triplets
+                result.add((e1,e2,rt))
+            else:
+                pass
+        return result
