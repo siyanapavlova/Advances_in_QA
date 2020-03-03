3473cb26285ea3b175ffdf0669b86d1096f04c0d
SimonPreissner <simon.preissner@gmx.de>
2020-03-03 21:09:22 +0100

    re-writing the Paragraph Selector

--- modules/ParagraphSelector.py
+++ modules/ParagraphSelector.py
@@ -8,55 +8,46 @@ from transformers import BertTokenizer, BertModel
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import os,sys,inspect
+from tqdm import tqdm
 from sklearn.metrics import recall_score, precision_score, f1_score
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 from utils import HotPotDataHandler
 
-def encode(text,
-           tokenizer=BertTokenizer.from_pretrained('bert-base-uncased'),
-           model=BertModel.from_pretrained('bert-base-uncased',
-                                 output_hidden_states=True,
-                                 output_attentions=True)):
-    ''' TODO: document
-    '''
-    
-    input_ids = torch.tensor([tokenizer.encode(text)])
-    all_hidden_states, all_attentions = model(input_ids)[-2:]
-    
-    # This is the embedding of the [CLS] token.
-    # [-1] is the last hidden state (list of sentences)
-    # first [0] - first (and only) sentence
-    # second [0] - first ([CLS]) token of the sentence
-    return all_hidden_states[-1][0][0]
 
 def make_training_data(data,
-                       tokenizer=BertTokenizer.from_pretrained('bert-base-uncased'),
-                       model=BertModel.from_pretrained('bert-base-uncased',
-                                 output_hidden_states=True,
-                                 output_attentions=True)):
-    '''
+                       tokenizer=BertTokenizer.from_pretrained('bert-base-uncased')):
+    """
+    #TODO docstring
     Make a dataframe with training data for selecting relevant paragraphs
     Each entry in the dataframe has three columns:
-        1. Query - the question
-        2. Paragraphs - the paragraphs
-        3. Label - 0 (unrelated) or 1 (related)
-    '''
+    1. id -- ID of the question
+    2. label -- 0 (unrelated) or 1 (related)
+    3. tokens -- token IDs of query and paragraph
+    :param data: question ID, supporting facts, question, and paragraphs
+    :type data: list(tuple(str, list(str), str, list(list(str, list(str)))))
+    :param tokenizer: default: BertTokenizer(bert-base-uncased)
+    :return: DataFame
+    """
+
     labels = []
     datapoints = []
-    for point in data:        
+    for point in tqdm(data):
         for para in point[3]:
-            labels.append(torch.Tensor([int(para[0] in point[1])])) # Label 1: if paragraph title is in supporting facts, otherwise 0
-            encoded_point = encode("[CLS] " + point[2] + " [SEP] " + ("").join(para[1]) + " [SEP]", tokenizer, model)
-            datapoints.append(encoded_point)
-        
-    df = pd.DataFrame({
-        'id': range(len(labels)),
-        'label': labels,
-        'text': datapoints
-    })
-    return df 
+            # Label is 1: if paragraph title is in supporting facts, otherwise 0
+            labels.append([int(para[0] in point[1])])
+            # automatically prefixes [CLS] and appends [SEP]
+            token_ids = tokenizer.encode( point[2] + " [SEP] " + ("").join(para[1]) ) #TODO limit to 512 tokens
+            datapoints.append(token_ids)
+
+    result = torch.Tensor(list(zip(datapoints, labels))) #TODO make this work (are tensors really the way to go?)
+    result = result.T
+    print(result.shape)
+    print(type(result))
+    input("shape and type of the data") #CLEANUP
+    print(result)
+    return result
 
 class ParagraphSelector():
     """
@@ -71,22 +62,40 @@ class ParagraphSelector():
         TODO: write docstring
         """
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased') if not tokenizer else tokenizer
-        self.encoder_model = BertModel.from_pretrained('bert-base-uncased',
-                                 output_hidden_states=True,
-                                 output_attentions=True) if not encoder_model else encoder_model
-        
+
         class ParagraphSelectorNet(torch.nn.Module):
             """
             TODO: write docstring
             """
             def __init__(self, input_size=768, output_size=1):
                 super(ParagraphSelectorNet, self).__init__()
+                self.encoder_model = BertModel.from_pretrained('bert-base-uncased',
+                                                               output_hidden_states=True,
+                                                               output_attentions=True) if not encoder_model else encoder_model
                 self.linear  = torch.nn.Linear(input_size, output_size)
 
-            def forward(self, embedding):
+            def forward(self, token_ids):
+
+                # [-2] is all_hidden_states
+                # [-1] is the last hidden state (list of sentences)
+                # first [0] - first (and only) sentence
+                # second [0] - first ([CLS]) token of the sentence
+                from pprint import pprint
+                pprint(token_ids)
+                print(type(token_ids))
+                input("type of token_ids")
+                token_ids = torch.tensor(list(token_ids))
+                print(type(token_ids[0]))
+                input("token_ids[0]")
+                print(type(self.encoder_model(token_ids)))
+                input("type of the encoder_model output")
+                print(self.encoder_model(token_ids).shape) #CLEANUP
+                input("shape of the encoder_model output")
+                print(self.encoder_model(token_ids)[-2][-1][0][0])
+                input("encoder_model CLS token?")
+                embedding = self.encoder_model(token_ids)[-2][-1][0][0]
                 output = self.linear(embedding)
                 output = torch.sigmoid(output)
-
                 return output 
             
         self.net = ParagraphSelectorNet()
@@ -95,21 +104,8 @@ class ParagraphSelector():
                 self.net.load_state_dict(torch.load(model_path))
             except FileNotFoundError as e:
                 print(e, model_path)
-    
-    def encode(self, text):
-        ''' TODO: document
-        '''
 
-        input_ids = torch.tensor([self.tokenizer.encode(text)])
-        all_hidden_states, all_attentions = self.encoder_model(input_ids)[-2:]
-
-        # This is the embedding of the [CLS] token.
-        # [-1] is the last hidden state (list of sentences)
-        # first [0] - first (and only) sentence
-        # second [0] - first ([CLS]) token of the sentence
-        return all_hidden_states[-1][0][0]
-
-    def train(self, train_data, labels, epochs, learning_rate=0.0001):
+    def train(self, train_data, labels, epochs=10, learning_rate=0.0001):
         """
         TODO: write docstring
         """
@@ -119,7 +115,10 @@ class ParagraphSelector():
         optimizer = torch.optim.Adam(self.net.parameters(), lr=learning_rate)
 
         losses = []
-        
+        inputs = train_data[1] # ["tokens"]
+        labels = train_data[2] # ["label"]
+
+
         # Set the network into train mode
         self.net.train()
 
@@ -130,13 +129,16 @@ class ParagraphSelector():
         print("Training...")
 
         # Iterate over the epochs
+        N = len(train_data)
         for epoch in range(epochs):
             print('Epoch %d/%d' % (epoch + 1, epochs))
-            for inputs, label in zip(train_data, labels):
+            for i in range(0, N, batch_size):
+                inputs = train_data[i:i+batch_size] # select one batch of data
+                labels = labels[i:i+batch_size] # and the corresponding labels
 
                 optimizer.zero_grad()
-                outputs = self.net(inputs)
-                loss = criterion(outputs, label)
+                outputs = self.net(inputs) # encode and apply linear layer
+                loss = criterion(outputs, labels)
                 loss.backward(retain_graph=True)
                 losses.append(loss.item())
                 optimizer.step()
@@ -173,12 +175,11 @@ class ParagraphSelector():
         return precision, recall, f1, ids, all_true, all_pred
     
     def predict(self, p):
-        """ Given the encoding of a paragraph (query + paragraph),
+        """
+        Given the encoding of a paragraph (query + paragraph),
         return the score that the model predicts for that paragraph
-        
-        Parameters: p - encoding of the paragraph (along with the query),
-                        as described in the paper
-        Output: score between 0 and 1 for that paragraph
+        :param p: encoding of the paragraph (along with the query), as described in the paper
+        :return: score between 0 and 1 for that paragraph
         """
         self.net.eval()
         score = self.net(p)
@@ -200,9 +201,12 @@ class ParagraphSelector():
                            ...]
         """
         context = []
+
         for p in datapoint[3]:
             # p[0] is the paragraph title, p[1] is the list of sentences in the paragraph
-            encoded_p = self.encode("[CLS] " + datapoint[2] + " [SEP] " + ("").join(p[1]) + " [SEP]")
+            encoded_p = torch.tensor([self.tokenizer.encode(
+                                      datapoint[2] + " [SEP] " + ("").join(p[1]) ) #TODO limit to 512 tokens
+                                     ])
             score = self.predict(encoded_p)
             if score > threshold:
                 context.append(p)
@@ -222,15 +226,18 @@ if __name__ == "__main__":
     print("Reading data...")
     dh = HotPotDataHandler(parent_dir + "/data/hotpot_train_v1.1.json")
     data = dh.data_for_paragraph_selector()
-    
+
     print("Splitting data...")
     training_data_raw, test_data_raw = train_test_split(data[:4], test_size=0.25, random_state=42, shuffle=True)
     training_data = make_training_data(training_data_raw)
     training_data = shuffle(training_data, random_state=42)
+
+    # TODO we use DataLoader to handle batching; is this the right way?
+    train_data = torch.utils.data.DataLoader(training_data, batch_size=1)
     
     print("Initilising ParagraphSelector...")
     ps = ParagraphSelector()
-    losses = ps.train(training_data["text"], training_data["label"], 1)
+    losses = ps.train(train_data, epochs=1)
     
     print("Saving model...")
     ps.save(parent_dir + '/models/paragraphSelector_all.pt')
