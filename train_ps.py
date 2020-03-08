50dcf8736d0afc4bb1a4cec83a548f043ab9ebe5
SimonPreissner <simon.preissner@gmx.de>
2020-03-08 23:00:32 +0100

    applied the recent changes to train_ps.py

--- train_ps.py
+++ train_ps.py
@@ -29,7 +29,10 @@ if __name__ == '__main__':
 
     model_abs_path = cfg('model_abs_dir') + args.model_name
     model_abs_path += '.pt' if not args.model_name.endswith('.pt') else model_abs_path
+    losses_abs_path = cfg("model_abs_dir") + "performance/" + args.model_name + ".losses"
+    traintime_abs_path = cfg("model_abs_dir") + "performance/" + args.model_name + ".times"
     take_time("parameter input")
+
 
 
     #========== DATA PREPARATION
@@ -39,30 +42,44 @@ if __name__ == '__main__':
     take_time("data loading")
 
     print("Splitting data...")
-    train_data_raw, test_data_raw = train_test_split(data,
+    data_limit = cfg("dataset_size") if cfg("dataset_size") else len(data)
+    train_data_raw, test_data_raw = train_test_split(data[:data_limit],
                                                      test_size=cfg('test_split'),
-                                                     random_state=cfg('data_shuffle_seed'),
+                                                     random_state=cfg('shuffle_seed'),
                                                      shuffle=True)
-    train_data = ParagraphSelector.make_training_data(train_data_raw)
-    train_data = shuffle(train_data, random_state=cfg('data_shuffle_seed'))
+    train_data = ParagraphSelector.make_training_data(train_data_raw,
+                                                      text_length=cfg("text_length"))
+    #train_data = shuffle(train_data, random_state=cfg('data_shuffle_seed')) #CLEANUP?
     take_time("data preparation")
 
 
     #========== TRAINING
     print("Initilising ParagraphSelector...")
     ps = ParagraphSelector()
-    take_time("paragraph selector initiation")
 
     print(f"training for {cfg('epochs')} epochs...")
-    losses = ps.train(train_data["text"],
-                      train_data["label"],
-                      cfg("epochs"))
-    take_time(f"training for {cfg('epochs')} epochs")
+    losses = ps.train(train_data,
+                      epochs=cfg("epochs"),
+                      batch_size=cfg("batch_size"),
+                      learning_rate=cfg("learning_rate"))
+    take_time(f"training")
 
     print(f"Saving model in {model_abs_path}...")
     ps.save(model_abs_path)
-    take_time("saving model")
 
-    take_time.total()
-    print("Times taken:")
-    print(take_time)
+    print(f"Saving losses in {losses_abs_path}...")
+    with open(losses_abs_path, "w") as f:
+        f.write("\n".join([str(l) for l in losses]))
+
+    print(f"Saving times taken to {traintime_abs_path}...")
+    with open(traintime_abs_path, 'w', encoding='utf-8') as f:
+        f.write("Configuration in: " + args.config_file + "\n")
+        f.write(str(cfg))
+
+        take_time("saving results")
+        take_time.total()
+        f.write("\n\nTimes taken:\n" + str(take_time))
+
+    print("\nTimes taken:\n", take_time)
+
+
