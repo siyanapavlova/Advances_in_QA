f6fc6a36e6c8d6eb909803e90ffbfcc62a63c083
SimonPreissner <simon.preissner@gmx.de>
2020-03-26 17:09:20 +0100

    docstrings for Encoder

--- modules/Encoder.py
+++ modules/Encoder.py
@@ -14,32 +14,38 @@ import torch.nn.functional as F
 
 class Encoder():
     """
-    TODO: write docstring
+    Use BERT and bidirectional attention flow (BiDAF) to encode a query and a
+    context. Both BERT and the BiDAF component are trained.
     """
 
-    def __init__(self,
-                 text_length=512,
-                 pad_token_id=0,
-                 tokenizer=None,
-                 encoder_model=None):
+    def __init__(self, text_length=512, pad_token_id=0, tokenizer=None, encoder_model=None):
         """
-        TODO: write docstring
+        Instantiate a Bert tokenizer and a BiDAF net which contains the BERT encoder.
+        Sizes of input and output (768,300) are not implemented to be changeable.
+        :param text_length: maximum number of tokens (query+context)
+        :param pad_token_id: for padding to text_length
+        :param tokenizer: defaults to 'bert-base-uncased'
+        :param encoder_model: defaults to 'bert-base-uncased'
         """
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased') if not tokenizer else tokenizer
         
         class BiDAFNet(torch.nn.Module):
             """
-            TODO: write docstring.
-
-            BiDAF paper: arxiv.org/pdf/1611.01603.pdf
-            There's a link to the code, but that uses TensorFlow
-
-            We adapted this implementation of the BiDAF
-            Attention Layer: https://github.com/galsang/BiDAF-pytorch
+            This class implements bidirectional attention flow (BiDAF) as
+            described in Seo et al. (2016): arxiv.org/pdf/1611.01603.pdf
+            The subsequent code is copied in most parts from Taeuk Kim's
+            Pytorch re-implementation of the above paper:
+            https://github.com/galsang/BiDAF-pytorch
             """
-            def __init__(self, hidden_size=768, output_size=300):
+            def __init__(self, hidden_size=768, output_size=300, encoder_model=None):
+                """
+                Make an encoder and attention layers as well as an output layer.
+                :param hidden_size: input (usually from a BERT encoder)
+                :param output_size: output (specified to 300)
+                :param encoder_model: defaults to 'bert-base-uncased'
+                """
                 super(BiDAFNet, self).__init__()
-                
+
                 self.encoder_model = BertModel.from_pretrained('bert-base-uncased',
                                  output_hidden_states=True,
                                  output_attentions=True) if not encoder_model else encoder_model
@@ -51,14 +57,20 @@ class Encoder():
                 self.reduction_layer = Linear(hidden_size * 4, output_size)
 
             def forward(self, q_token_ids, c_token_ids, batch=1):
-                #TODO docstring
+                """
+                Encode a query and a context (both lists of token IDs) and
+                apply BiDAF to the encoding.
+                :param q_token_ids: list[int] - obtained from a tokenizer
+                :param c_token_ids: list[int] - obtained from a tokenizer
+                :return: encoded and BiDAF-ed context of shape (batch, c_len, output_size)
+                """
 
                 def att_flow_layer(q, c):
                     """
-                    perform bidrectional attention flow and return the updated context
-                    :param c: (batch, c_len, hidden_size)
-                    :param q: (batch, q_len, hidden_size)
-                    :return: (batch, c_len, output_size)
+                    Perform BiDAF and return the updated context.
+                    :param q: encoded query of shape (batch, q_len, hidden_size)
+                    :param c: encoded context of shape (batch, c_len, hidden_size)
+                    :return: encoded context (batch, c_len, output_size)
                     """
                     c_len = c.size(1)
                     q_len = q.size(1)
@@ -76,15 +88,12 @@ class Encoder():
                         cq
 
                     a = F.softmax(s, dim=2) # (batch, c_len, q_len)
-
                     # (batch, c_len, q_len) * (batch, q_len, hidden_size) -> (batch, c_len, hidden_size)
                     c2q_att = torch.bmm(a, q)
 
                     b = F.softmax(torch.max(s, dim=2)[0], dim=1).unsqueeze(1)  # (batch, 1, c_len)
-
                     # (batch, 1, c_len) * (batch, c_len, hidden_size) -> (batch, hidden_size)
                     q2c_att = torch.bmm(b, c).squeeze(1)
-
                     # (batch, c_len, hidden_size) (tiled)
                     q2c_att = q2c_att.unsqueeze(1).expand(-1, c_len, -1)
 
@@ -98,26 +107,24 @@ class Encoder():
                 len_context = len(c_token_ids)
                 
                 all_token_ids = q_token_ids + c_token_ids
-                
-                
-                # Add padding if there are fewer than text_length tokens,
-                # else trim to text_length
+
+                # Add padding or trim to text_length
                 if len(all_token_ids) < text_length:
                     all_token_ids += [pad_token_id for _ in range(text_length - len(all_token_ids))]
                 else:
                     all_token_ids = all_token_ids[:text_length]                
-                
+
+                # get the embeddings corresponding to the token IDs
                 all_hidden_states, all_attentions = self.encoder_model(torch.tensor([all_token_ids]))[-2:]
                 
                 # Next five lines: 
                 # This is the embedding of the context + query
-                # [-1] stands for the last hidden state
-                # [0] is the first sentence
-                # sentence being defined as a sequence of characters, and not a linguistic sentence)
+                # [-1] = last hidden state
+                # [0] = first sentence ('sentence' = sequence of characters)
                 q_emb = all_hidden_states[-1][0][:len_query]
                 
-                # If query + context is longer than text_lengh (512 by default),
-                # the contenxt embedding includes everything except from the query
+                # If query + context is longer than text_length (512 by default),
+                # the context embedding includes everything except the query
                 if len(all_token_ids) > text_length:
                     c_emb = all_hidden_states[-1][0][len_query:]
                 # Else (query + context shorter than text_length),
@@ -128,23 +135,23 @@ class Encoder():
                 else:
                     c_emb = all_hidden_states[-1][0][len_query:len_query+len_context]
                 
-                print("Query shape:", q_emb.shape)
-                print("Context shape:", c_emb.shape)
-                print(q_emb)
-                print(c_emb)
-                
-                return #TODO remove this
+                #print("Query shape:", q_emb.shape) #CLEANUP?
+                #print("Context shape:", c_emb.shape)
+                #print(q_emb)
+                #print(c_emb)
             
                 g = att_flow_layer(q_emb, c_emb)
                 return g
         
-        self.net = BiDAFNet()
+        self.net = BiDAFNet(encoder_model=encoder_model)
 
-    def encode(self,
-               query=None,
-               context=None):
+    def encode(self, query=None, context=None):
         """
-        TODO: write docstring
+        return the token IDs of a query and a context
+        :param query: str
+        :param context: setences, paragraphs, paragraph titles
+        :type context: list[list[str,list[str]]]
+        :return:
         """
         if not query:
             print("No query for Encoder. Working with toy example.")
@@ -162,8 +169,8 @@ class Encoder():
                   " Well, I also think that he is nice.",
                   " Mary, however liked Tony even more than we do."]]
             ]
-            
-        
+
+
         # Tokenize and encode the query and the context
         query_input_ids = self.tokenizer.encode(query,
                                                 add_special_tokens=False,
@@ -171,7 +178,7 @@ class Encoder():
         context_input_ids = self.tokenizer.encode(flatten_context(context),
                                                   add_special_tokens=False,
                                                   max_length=512)
-        
+
         return query_input_ids, context_input_ids
     
     def train(self, q_token_ids, c_token_ids):
